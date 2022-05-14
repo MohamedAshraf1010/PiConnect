04971d337e83cef082c5acf930b2b22dee5b973d
mohamed <mohamed.ashraf.10107@gmail.com>
2022-05-14 16:50:50 +0200

    Commit #5: Restful GET.

--- src/main/kotlin/domain/Get.kt
+++ src/main/kotlin/domain/Get.kt
@@ -6,5 +6,5 @@ class Get constructor(private val api: String) {
 
     private val networkConnector: NetworkConnector = NetworkConnectorImplementation
 
-    fun <T> run(headers: Map<String, String>, queries: Map<String, String>): T = networkConnector.connect(api = api, headers = headers, queries = queries)
+    fun <T> run(headers: Map<String, String>, queries: Map<String, String>, responseType: Class<T>): T = networkConnector.connect(api = api, headers = headers, queries = queries, type = responseType)
 }
